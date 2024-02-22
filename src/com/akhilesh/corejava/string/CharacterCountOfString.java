eee6484a1ee173fab56356c1c3890e78bb392dee
Akhilesh <ak2bitm@gmail.com>
2024-02-22 14:36:47 +0530

    char count commit

--- src/com/akhilesh/corejava/string/CharacterCountOfString.java
+++ src/com/akhilesh/corejava/string/CharacterCountOfString.java
@@ -1,5 +1,17 @@
 package com.akhilesh.corejava.string;
 
 public class CharacterCountOfString {
-
+    static void characterCount(String str){
+        int count = 0;
+        for (int i=0; i<=str.length()-1; i++){
+            if (str.charAt(i)== 'a' || str.charAt(i)=='A'){
+                count++;
+            }
+        }
+        System.out.println("Number of Character a or A is present in string are :"+count);
+    }
+    public static void main(String[] args) {
+        String str = "Akhilesh kumar patel";
+        characterCount(str);
+    }
 }
